6a772247b459d0bc1662c66a7e9a6bd9c99abf93
Ritam Nag <rnag@mit.edu>
2022-07-28 01:24:01 +0530

    https fix

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const SOCKET_ADDR = process.env.NODE_ENV === "production" ? 
-    "time-travel-chess-server.herokuapp.com" : "localhost:3000";
+    "https://time-travel-chess-server.herokuapp.com" : "localhost:3000";
